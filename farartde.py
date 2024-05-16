de809d99edf6d60983d49722806c1f8c91e55fbd
BogziCB <bogdanchirita26@gmail.com>
2024-05-16 15:52:13 +0300

    farartde added and obstacle added to test file"

--- farartde.py
+++ farartde.py
@@ -16,15 +16,15 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 from collections import deque
 
+'''
 import rtde_control
 import rtde_receive
+'''
 
 #initializare world
 world = World(stage_units_in_meters=1.0)
 world.scene.add_default_ground_plane()
 
-#f_real = open("/home/bogdan/Desktop/poz/poz_real.txt", "w")
-#f_virt = open("/home/bogdan/Desktop/poz/poz_virt.txt", "w")
 #nume robot
 robot_name = "UR3e"
 
@@ -48,9 +48,15 @@ rmpflow = RmpFlow(**rmp_config)
 articulation_rmpflow = ArticulationMotionPolicy(robot, rmpflow, 1.0/60)
 articulation_controller = robot.get_articulation_controller()
 
+obstacle = cuboid.VisualCuboid(
+    "/World/obstacle", position=np.array([-0.04921, 0.45526, 0.1]), color=np.array([0, 1.0, 0]), size=0.1, scale=np.array([4, 4, 5])
+)
+rmpflow.add_obstacle(obstacle)
+
 world.reset()
 #rtde_receive
 
+'''
 robot_rtde_control = rtde_control.RTDEControlInterface("169.254.233.2")
 robot_rtde_receive = rtde_receive.RTDEReceiveInterface("169.254.233.2")
 real_robot_pose = robot_rtde_receive.getActualQ()
@@ -64,32 +70,20 @@ end_effec_quat_sh = end_effec_rot_matr.as_quat()
 end_effec_quat_r = deque(end_effec_quat_sh)
 end_effec_quat_r.rotate(1)
 end_effec_quat = list(end_effec_quat_r)
-
+'''
 target_cube = cuboid.VisualCuboid(
-    "/World/target", position=end_effec_position, orientation=end_effec_quat, color=np.array([0, 1, 1]), size=0.1, scale=np.array([0.1,0.1,0.1])
+    "/World/target", position=[0.5,0.5,0.2], orientation=[0,0,1,0], color=np.array([0, 1, 1]), size=0.1, scale=np.array([0.1,0.1,0.1])
 )
-
-band_one = cuboid.FixedCuboid(
-    "/World/band_one", position=np.array([0.14, 0.379, 0]), size=0.1, scale=np.array([7, 1.5, 3.63])
-)
-rmpflow.add_obstacle(band_one)
-
-band_two = cuboid.VisualCuboid(
-    "/World/band_two", position=np.array([0.24, -0.152, 0.05]), size=0.1, scale=np.array([3.77, 1.33, -1.67])
-)
-rmpflow.add_obstacle(band_two)
-
-#poz_real = list()
-#poz_virt = list()
 
 while simulation_app.is_running():
     if world.is_playing():
         if world.current_time_step_index == 0:
             world.reset()
         
+        
         #urmarire cub
         rmpflow.set_end_effector_target(target_position=target_cube.get_world_pose()[0], target_orientation=target_cube.get_world_pose()[1])
-
+        '''
         velocity = 0.1
         acceleration = 0.1
         dt = 1.0/500  # 2ms
@@ -102,22 +96,17 @@ while simulation_app.is_running():
         robot_rtde_control.servoJ(joint, velocity, acceleration, dt, lookahead_time, gain)
 
         robot_rtde_control.waitPeriod(t_start)
-        
+        '''
         rmpflow.update_world()
         actions = articulation_rmpflow.get_next_articulation_action()
         articulation_controller.apply_action(actions)
-
-        robot.set_joint_positions(np.array(robot_rtde_receive.getActualQ()))
-
-        #f_real.write(str(robot_rtde_receive.getActualQ()))
-        #f_virt.write(str(robot.get_joint_positions()))
-
+        
     if world.is_stopped():
-        #f_real.close()
-        #f_virt.close()
+        '''
         robot_rtde_control.servoStop()
         robot_rtde_control.stopScript()
         robot_rtde_receive.disconnect()
+        '''
         simulation_app.close()
 
     world.step(render=True)
